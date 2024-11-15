46b6014466f52d473dd90ffb2e6aaacd80481dcc
Simon Jacobsson <simon.jacobsson@chasacademy.se>
2024-11-15 06:06:56 +0100

    Changed html mostly. JS localstorage
    changed html since it got confusing, made it simpler and redid JS since syntax and statements were wrong, still testing...

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const titleText = document.getElementById("titleText");
+  const noteForm = document.getElementById("noteForm");
+  const saveButton = document.getElementById("saveButton");
+
+  function display() {
+    localStorage.setItem("saveNote", noteForm.value, titleText.value);
+    console.log(localStorage.getItem("saveNote"));
+
+    //   h2.innerhtml = input.value;
+    //   h2.innerhtml = localStorage.getItem("saveTitle");
+  }
+
   //låter av hemsidan för att ladda klart så inga DOM element körs innan de ska
-  const titleInput = document.getElementById("titleInput");
-  const contentInput = document.getElementById("contentInput");
-  const noteSave = document.querySelector("saveNote");
+  //   const titleInput = document.getElementById("titleInput");
+  //   const contentInput = document.getElementById("contentInput");
+  //   const saveNote = document.querySelector("buttonSave");
 
+  //   //   let title = titleInput.trim();
+  //   //   let content = contentInput.trime(); //oavsett value (siffra eller bokstav så skalar den av överbliven whitespace)
+  //   // document.addEventListener("click", saveNote);
+  //   let buttonSave = document.getElementById("buttonSave");
 
-  const title = titleInput.value.trim();
-  const content = content.value.trime(); //oavsett value (siffra eller bokstav så skalar den av överbliven whitespace)
+  //   localStorage.setItem("buttonSave", input.value);
+  //   h2.innerHTML = localStorage.getItem("buttonSave");
+  //   console.log(buttonSave);
 
+  //   //   {
+  //   //     if (!title || !content = null){
+  //   //     alert("Du måste fylla i både titel och anteckningar för att fortsätta");}
+  //   //     else{
+  //   //         return;
+  //   //     };
 
-  saveNote.addEventListener("submit", (click) => {  
-    if (title || content = null){
-    alert("Du måste fylla i både titel och anteckningar för att fortsätta");}
-    else{
-        return;
-    };
-    
-  });
+  //   //   });
 });
