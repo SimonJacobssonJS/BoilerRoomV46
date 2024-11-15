62230e42e94a9d6a0c8067694a6b4a1692d5ad5d
Simon Jacobsson <simon.jacobsson@chasacademy.se>
2024-11-15 04:11:34 +0100

    update script.js  Testing around
    playing around with the buttons i got on my html. saveNote needed queryselector or to be made an id

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-let createFile = "skapade alla viktiga filer för uppgiften";
+document.addEventListener("DOMContentLoaded", () => {
+  //låter av hemsidan för att ladda klart så inga DOM element körs innan de ska
+  const titleInput = document.getElementById("titleInput");
+  const contentInput = document.getElementById("contentInput");
+  const noteSave = document.querySelector("saveNote");
+
+
+  const title = titleInput.value.trim();
+  const content = content.value.trime(); //oavsett value (siffra eller bokstav så skalar den av överbliven whitespace)
+
+
+  saveNote.addEventListener("submit", (click) => {  
+    if (title || content = null){
+    alert("Du måste fylla i både titel och anteckningar för att fortsätta");}
+    else{
+        return;
+    };
+    
+  });
+});
