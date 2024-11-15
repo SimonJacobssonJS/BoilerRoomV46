a6345500063cd7e5887712eddc42676006677f59
Simon Jacobsson <simon.jacobsson@chasacademy.se>
2024-11-15 09:38:04 +0100

    Wrong code format
    needed a little change in html to make it correct. Than tried some functions in JS file, will continue....ofc

--- script.js
+++ script.js
@@ -7,28 +7,40 @@ document.addEventListener("DOMContentLoaded", () => {
   const saveButton = document.getElementById("saveButton");
   const clearButton = document.getElementById("clearButton");
 
-  //const notes = JSON.parse(localStorage.getItem(".notes")) || [];
+  const notes = JSON.parse(localStorage.getItem(".notes")) || [];
 
-  const addNotes = (titleText, noteForm) => {};
+  const addNotes = (titleText, noteForm) => {
+    notes.push({
+      titleText,
+      noteForm,
+    });
+    return { titleText, noteForm };
+  };
 
-  const createNotes = ({ title, noteForm }) => {
+  const createNotes = ({ titleText, noteForm }) => {
     const noteCreateDiv = document.createElement("div");
-    const titleh2 = document.createElement("h2");
+    const titleH2 = document.createElement("h2");
     const noteParagraph = document.createElement("p");
 
-    createNotes.innerText = addNotes;
+    titleH2.innerText = addNotes;
+    noteParagraph.innerText = addNotes;
 
-    noteCreateDiv.append(titleh2, noteParagraph);
+    noteCreateDiv.append(titleH2, noteParagraph);
     notesContainer.appendChild(notesContainer);
-
-    const notes = [];
-    notes.forEach(noteCreateDiv);
-    noteForm.onsubmit = (e) => {
-      e.preventDefault();
-    };
   };
-  //notesContainer.push({ createNotes });
+  notes.forEach(noteCreateDiv);
+  noteForm.onsubmit = (e) => {
+    e.preventDefault();
+
+    const newNotes = addNotes(titleText.value, noteForm.value);
+  };
+
+  noteCreateDiv(addNotes);
+  titleText.value = "";
+  noteForm.value = "";
 
   localStorage.setItem("createNotes", JSON.stringify(notesContainer));
   return { titleText, noteForm };
 });
+
+//notesContainer.push({ createNotes });
